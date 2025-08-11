50bf1ca68d540e86b18cc25f0422fafec47b2c26
Brynn Park <pbh7080@gmail.com>
2025-08-11 12:54:11 +0900

    First commit to deploy

---
+++ terraform/variables.tf
@@ -0,0 +1,17 @@
+
+# Project Specific Variables
+variable "static_bucket_name" {
+  type        = string
+  description = "The name of S3 bucket for static website hosting"
+  default = "brynnpark-web"
+}
+
+// Create a variable for our domain name because we'll be using it a lot.
+variable "www_domain_name" {
+  default = "www.brynnpark.cloud"
+}
+
+// We'll also need the root domain (also known as zone apex or naked domain).
+variable "root_domain_name" {
+  default = "brynnpark.cloud"
+}
